b3d5106aa07b3641007ec4c73aade300648f268d
Dallasss7 <mr.dalsummers@gmail.com>
2019-10-20 20:02:00 -0700

    created side nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component, HostListener, ViewChild } from "@angular/core";
+import { Component, HostListener, ViewChild, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { MatIconRegistry } from "@angular/material";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild("nav") public nav;
   public title = "Operation Turkey Meats";
+  public isMobile: boolean;
   @HostListener("window:scroll", ["$event"])
   onScroll(event) {
     if (window.pageYOffset > 150) {
@@ -18,7 +21,17 @@ export class AppComponent {
     }
   }
 
-  constructor(public router: Router) {}
+  constructor(public router: Router, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+        'menu',
+        sanitizer.bypassSecurityTrustResourceUrl('assets/menu-24px.svg'));
+  }
+
+  ngOnInit() {
+	  if (window.innerWidth < 600) {
+		this.isMobile = true;
+	  }
+  }
 
   public redirectDonate() {
     console.log("HERE");
